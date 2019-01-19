5275f28e7efef41384254d5ff2e1aab903030a3d
Greg <gregpaton08@gmail.com>
2019-01-19 12:53:00 -0500

    Updates API to support getting books

--- server/server.js
+++ server/server.js
@@ -8,3 +8,17 @@ app.listen(3000, () => {
 app.get("/url", (req, res, next) => {
   res.json([ 1, 2, 3, 4, 5 ])
 })
+
+const dateToJson = (year, month, date) => (new Date(year, month - 1, date)).toJSON()
+
+var books = [
+  {
+    title: 'Grit',
+    author: 'Angela Duckworth',
+    dateCompleted: dateToJson(2019, 1, 4)
+  }
+]
+
+app.get("/books", (req, res, next) => {
+  res.json(books)
+})
