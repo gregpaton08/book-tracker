68a7ecebb1b3c9de00f05b70374d2440c0f0ccdf
Greg <gregpaton08@gmail.com>
2019-01-23 22:53:40 -0500

    Adds mongodb to store data

--- server/server.js
+++ server/server.js
@@ -3,29 +3,40 @@ var bodyParser = require('body-parser')
 var app = express()
 app.use(bodyParser.json())
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+var mongodb = require('mongodb')
+var MongoClient = mongodb.MongoClient
+
+var url = 'mongodb://localhost:27017/'
+
+var collection
+
+MongoClient.connect(url, (err, client) => {
+  err ? console.error(err) : console.log('connected to database server successfully', db)
+
+  var db = client.db('book_tracker')
+  collection = db.collection('books')
 })
 
-app.get("/url", (req, res, next) => {
-  res.json([ 1, 2, 3, 4, 5 ])
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
 })
 
 const dateToJson = (year, month, date) => (new Date(year, month - 1, date)).toJSON()
 
-var books = [
-  {
-    title: 'Grit',
-    author: 'Angela Duckworth',
-    dateCompleted: dateToJson(2019, 1, 4)
-  }
-]
-
 app.get("/books", (req, res, next) => {
-  res.json(books)
+  collection.find().toArray(function(error, books) {
+    res.json(books)
+  })
 })
 
 app.post('/books', (req, res) => {
-  console.log(req.body)
-  res.sendStatus(200)
+  collection.insertOne(req.body, (err, r) => {
+    res.sendStatus(200)
+  })
+})
+
+app.delete('/books/:id', (req, res) => {
+  collection.deleteOne({ _id: new mongodb.ObjectID(req.params.id) }, (err, r) => {
+    res.sendStatus(r.deletedCount === 1 ? 200 : 404)
+  })
 })
