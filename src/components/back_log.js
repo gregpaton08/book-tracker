006fc9d1f5bd16b174d858cd2c68dad2b082d451
Gregory Paton <gregpaton08@gmail.com>
2019-07-07 10:57:20 -0400

    Adds a search field to the book list

--- src/components/back_log.js
+++ src/components/back_log.js
@@ -3,11 +3,29 @@ import PropTypes from 'prop-types'
 import BookList from '../containers/book_list'
 
 export class BackLog extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
   render() {
     return (
       <div className='book-list-container'>
         <h2>Back Log</h2>
-        <input type='text' placeholder='search' />
+        <input
+          type='text'
+          placeholder='search'
+          onChange={(event) => {
+            const searchTerm = event.target.value
+            this.setState(state => ({
+              ...state,
+              searchTerm
+            }))
+          }}
+        />
         <BookList />
       </div>
     )
@@ -15,5 +33,5 @@ export class BackLog extends React.Component {
 }
 
 BackLog.propTypes = {
-  
+
 }
