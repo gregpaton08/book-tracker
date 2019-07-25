af6f91ba4293f30f5c015e59967f28e3fecc8f0e
Gregory Paton <gregpaton08@gmail.com>
2019-07-25 19:27:18 -0400

    Fixes bug when refreshing book details page and book info disappears

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,18 +1,19 @@
 import { connect } from 'react-redux'
 import { BookDetail } from '../components/book_detail'
-import { updateBookStatus } from '../actions/books'
+import { fetchBooks, updateBookStatus, deleteBook } from '../actions/books'
 
 const mapStateToProps = (state, props) => {
   const params = new URLSearchParams(props.location.search)
   const bookId = params.get('id')
-  const book = state.books.books ? state.books.books[bookId] : {}
+  const book = state.books.books ? state.books.books[bookId] : undefined
   return {
     book
   }
 }
 
 const mapDispatchToProps = {
-  updateBookStatus
+  updateBookStatus,
+  fetchBooks
 }
 
 const BookDetailContainer = connect(
